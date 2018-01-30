9a361ddd0478703e042cccb6e1345eaa95aa7f18
A. Simeonov <angel.st.sim@gmail.com>
2018-01-30 20:44:03 +0000

    Added a scene for the simple minigame and some tests sprites to test aspect ratios

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    //get dropdown objects
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    public GameObject playerThree;
+    public GameObject playerFour;
+    public GameObject errorPanel;
+
+    public void StartNewGame()
+    {
+
+      //  StartCoroutine(ShowPopUpMessage(2));
+    }
+
+
+    IEnumerator ShowPopUpMessage(float delay)
+    {
+        errorPanel.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        errorPanel.SetActive(false);
+    }
+
+
+}
