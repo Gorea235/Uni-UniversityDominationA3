545b1af585984ca8205a6c0b0f2e93b20a0e756a
bnp502 <32845119+bnp502@users.noreply.github.com>
2018-01-31 18:12:14 +0000

    Implemented "GetPlayerTypes"

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,18 +6,30 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour {
 
     //get dropdown objects
-    public GameObject playerOne;
-    public GameObject playerTwo;
-    public GameObject playerThree;
-    public GameObject playerFour;
+    public GameObject playerOneType;
+	public GameObject playerOneColour;
+    public GameObject playerTwoType;
+	public GameObject playerTwoColour;
+    public GameObject playerThreeType;
+	public GameObject playerThreeColour;
+    public GameObject playerFourType;
+	public GameObject playerFourColour;
+	public GameObject startButton;
     public GameObject errorPanel;
 
     public void StartNewGame()
     {
-
+		List<int> types;
+		types = GetPlayerTypes();
+		Debug.Log ("Type values: " +  types[0].ToString() +","+ types[1].ToString()+","
+			+ types[2].ToString()+","+ types[3].ToString());	
       //  StartCoroutine(ShowPopUpMessage(2));
     }
 
+	public void ColourDropdownUpdate()
+	{
+	  //  Update dropdowns to disable duplicate colours	
+	}
 
     IEnumerator ShowPopUpMessage(float delay)
     {
@@ -26,5 +38,22 @@ public class MainMenu : MonoBehaviour {
         errorPanel.SetActive(false);
     }
 
+	public List<int> GetPlayerTypes()
+	{
+		List<int> types;
+		Dropdown One = playerOneType.GetComponent<Dropdown>();
+		Dropdown Two = playerTwoType.GetComponent<Dropdown>();
+		Dropdown Three = playerThreeType.GetComponent<Dropdown>();
+		Dropdown Four = playerFourType.GetComponent<Dropdown> ();
+	
+		types = new List<int>(new int[] {One.value, Two.value, Three.value, Four.value});
+		return types;
+	}
 
+	public List<int> GetPlayerColours()
+	{
+		List<int> colours;
+		colours = new List<int> ();
+		return colours;
+	}
 }
