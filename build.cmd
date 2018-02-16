b66f6f25927cb53fb15c4a6124825e10e7b05acf
Luke Davies <lkdvs3@gmail.com>
2018-02-16 11:38:01 +0000

    Fixed build script log output

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Building project...
-"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -runEditorTests -logFile "Build/build.log" -quit -executeMethod ProjectBuilder.BuildProject
+"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -runEditorTests -logFile "Builds/build.log" -quit -executeMethod ProjectBuilder.BuildProject
 echo Project Built!
 pause
